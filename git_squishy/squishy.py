361f5895624feee7a9a2cb33e8c39cf124d48346
Romanos Skiadas <rom.skiad@gmail.com>
2016-08-12 22:02:49 +0300

    Implement assert_branch_exists, change skeleton

--- git_squishy/squishy.py
+++ git_squishy/squishy.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 
 import sys
+import subprocess
 
 
 class SquishyException(Exception):
@@ -22,12 +23,22 @@ class NoSuchBranchException(SquishyException):
 
 def get_current_branch():
     """Return the current branch's name."""
+    # current_branch = git rev-parse --abbrev-ref HEAD
+    # assert_branch_exists(branch)
+    # return current_branch
     return "test"
 
 
-def check_branch_exists(branch):
+def assert_branch_exists(branch):
     """Check if a branch exists, exit if not."""
-    pass
+    cmd = ['git', 'rev-parse', '--verify', branch]
+    p = subprocess.Popen(cmd,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT
+                         )
+    (out, _) = p.communicate()
+    if (p.returncode != 0):
+        sys.exit(NoSuchBranchException.EXIT_CODE)
 
 
 def get_base_branch():
@@ -40,21 +51,24 @@ def get_base_branch():
         sys.exit(NoBaseBranchException.EXIT_CODE)
     else:
         base_branch = sys.argv[1]
-        check_branch_exists(base_branch)
+        assert_branch_exists(base_branch)
 
     return base_branch
 
 
-def get_diverged_commits(base_branch):
+def get_diverged_commits(current_branch, base_branch):
     """Get the number of commits diverged from `base_branch`."""
+    # git log base_branch..current_branch --pretty=oneline | count
+    return 0
 
 
-def squash(current_branch, base_branch):
+def squash(current_branch, commit_n):
+    """Squash $commit_n commits on current_branch."""
     pass
 
 
 def main():
     base_branch = get_base_branch()
     current_branch = get_current_branch()
-    get_diverged_commits(base_branch)
-    squash(current_branch, base_branch)
+    commit_number = get_diverged_commits(current_branch, base_branch)
+    squash(current_branch, commit_number)
